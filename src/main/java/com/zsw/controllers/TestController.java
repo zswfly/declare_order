6474408a39df1743e1865902b5726118ec60a8b3
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-04-09 14:57:06 +0800

    new

---
+++ src/main/java/com/zsw/controllers/TestController.java
@@ -0,0 +1,11 @@
+package com.zsw.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhangshaowei on 2020/4/2.
+ */
+@Controller
+public class TestController {
+
+}
